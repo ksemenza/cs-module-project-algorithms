d34c2f64e1b1a1efa09f24bcba7482fd3dca2c35
ksemenza <ksemenza13@gmail.com>
2020-07-23 15:44:07 -0400

    updated two tasks;

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -5,6 +5,17 @@ Returns: a List of integers
 
 
 def moving_zeroes(arr):
+    zeros = arr.count(0)
+    count = 1
+    while count <= zeros:
+        i = arr.index(0)
+        if arr[-count] != 0:
+            arr[i], arr[-count] = arr[-count], arr[i]
+
+        return arr
+
+
+def moving_zeroes_new(arr):
     # Your code here
     resArr = []
     for i in range(0, len(arr)):
@@ -21,3 +32,4 @@ if __name__ == '__main__':
     arr = [0, 3, 1, 0, -2]
 
     print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
+    print(f"The resulting of moving_zeroes_new is: {moving_zeroes_new(arr)}")
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -10,14 +10,21 @@ def single_number(arr):
     for i in range(0, len(arr)-1):
         appeared = 1
         for k in range(0, len(arr)-1):
-            # dont check against self
+            # check array values against it self
             if i != k:
-                # does i have a pair?
+                # check for pairs
                 if arr[i] == arr[k]:
-                    # if we find it's mate
+                    # if pair is found 
                     appeared += 1
         if appeared != 2:
             return arr[i]
+
+
+def single_number_new(arr):
+    result = 0
+    for i in arr:
+        result ^= i
+    return result
 
 
 if __name__ == '__main__':
@@ -25,3 +32,4 @@ if __name__ == '__main__':
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
 
     print(f"The odd-number-out is {single_number(arr)}")
+    print(f"The odd-number-out in new is {single_number_new(arr)}")
